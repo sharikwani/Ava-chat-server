d8850433158c029f7b1d363238c3d41d6055909d
sharikwani <80133446+sharikwani@users.noreply.github.com>
2025-11-29 20:37:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,3 @@
-import eventlet
-eventlet.monkey_patch()
-
 import os
 import time
 import random
@@ -256,6 +253,7 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     # This runs the production-ready Eventlet server
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
+
 
 
 
